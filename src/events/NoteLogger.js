4017722277e37236d115907696ca779b5067ed8b
Alexander Fang <alex.s.fang@gmail.com>
2026-02-25 11:05:11 -0800

    Created NoteLogger, which records improvisations in written format.
    - Listens for custom 'notestart' and 'noteend' events.
    - Logs with timing and position info from TimingEngine,

---
+++ src/events/NoteLogger.js
@@ -0,0 +1,73 @@
+/**
+ * NoteLogger listens for custom note events ('notestart' | 'noteend') dispatched by input handlers, and logs them.
+ * Uses TimingEngine as central clock.
+ */
+
+export class NoteLogger {
+    constructor(timingEngine) {
+        // Use TimingEngine as central clock
+        this.timingEngine = timingEngine;
+        this.loggingStartTime = null;
+        this.events = [];
+
+        this.handleNoteEvent = this.handleNoteEvent.bind(this);
+    }
+
+    /**
+     * Start logging note events. Listens for 'notestart' and 'noteend' events dispatched by KeyboardHandler.
+     * (TODO: extend to other input types).
+     */
+    start() {
+        this.loggingStartTime = this.timingEngine.getCurrentTime();
+        this.events = []; // Clear previous logs
+        
+        document.addEventListener('notestart', /** @param {CustomEvent} e */ (e) => {
+            this.handleNoteEvent(e);
+        });
+        document.addEventListener('noteend', /** @param {CustomEvent} e */ (e) => {
+            this.handleNoteEvent(e);
+        });
+    }
+
+    /**
+     * Stop logging, remove event listeners, and return collected events.
+     * @returns 
+     */
+    stop() {
+        document.removeEventListener('notestart', this.handleNoteEvent);
+        document.removeEventListener('noteend', this.handleNoteEvent);
+        return this.events;
+    }
+
+    /**
+     * Collects note events with timing, then logs them.
+     * @param {CustomEvent & {type: 'notestart'|'noteend'}} e note event from KeyboardHandler
+     * (TODO: extended to include other inputs)
+     */
+    handleNoteEvent(e) {
+        const timestamp = this.timingEngine.getCurrentTime() - this.loggingStartTime;
+        const { midiNumber, uniqueID } = e.detail;
+        const position = this.timingEngine.getCurrentPosition();
+
+        this.logEvent(timestamp, e.type, midiNumber, uniqueID, position);
+    }
+
+    /**
+     * 
+     * @param {number} timestamp elapsed time (in seconds with sub-millisecond precision) from backing track start
+     * @param {'notestart' | 'noteend'} eventType 
+     * @param {number} midiNumber
+     * @param {string} inputID key code that played the note
+     * @param {*} position additional contextual information from TimingEngine
+     * (TODO: extend to other input types)
+     */
+    logEvent(timestamp, eventType, midiNumber, inputID, position) {
+        this.events.push({
+            timestamp,
+            eventType,
+            inputID,
+            midiNumber,
+            position
+        });
+    }
+}
