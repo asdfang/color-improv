065679b3967418475f8499a4dac702856b4dac6f
Alexander Fang <alex.s.fang@gmail.com>
2026-03-01 12:53:46 -0800

    feat: add express hello world server

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3001;
+
+app.get('/api/health', (req, res) => {
+    // res.json({ status: 'ok', message: 'Server is running!' });
+    res.send('Hello world.');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
