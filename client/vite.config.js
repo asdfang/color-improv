d01535359f0b57727282a651303c69c671c5ab85
Alexander Fang <alex.s.fang@gmail.com>
2026-03-05 14:04:54 -0800

    build: adjust Vite proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,10 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3001'
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      }
     }
   }
 })
