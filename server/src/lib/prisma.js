055ab0290d1bbe35a08006926e94d90dbe37c11c
Alexander Fang <alex.s.fang@gmail.com>
2026-03-11 17:16:43 -0700

    fix: correct named imports

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,7 +1,7 @@
 import "dotenv/config"; // if not in index.js
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
-import requireEnv from '../utils/env.js';
+import { requireEnv } from '../utils/env.js';
 
 const connectionString = requireEnv('DATABASE_URL');
 
--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import requireEnv from './env.js';
+import { requireEnv } from './env.js';
 
 const JWT_SECRET = requireEnv('JWT_SECRET');
 const JWT_EXPIRES_IN = '7d';
