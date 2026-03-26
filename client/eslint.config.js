7e0a29c1abb99fef73bc135a91233944b63fc8b0
Alexander Fang <alex.s.fang@gmail.com>
2026-03-25 22:48:34 -0700

    build: configure linting for .jsx files

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import { reactRefresh } from "eslint-plugin-react-refresh";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,jsx,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   reactHooks.configs.flat.recommended,
   reactRefresh.configs.recommended(),
 ]);
