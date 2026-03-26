951852a847b0ab853c013446f8de8b1a1452312d
Alexander Fang <alex.s.fang@gmail.com>
2026-03-25 22:25:57 -0700

    build: add eslint react-hooks and react-refresh plugins

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,7 +1,11 @@
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import { reactRefresh } from "eslint-plugin-react-refresh";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.recommended(),
 ]);
