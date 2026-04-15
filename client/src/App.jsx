25fcb1328bd1cefe71e9eb0eeccc7c844f289a6e
Alexander Fang <alex.s.fang@gmail.com>
2026-04-14 22:11:21 -0700

    feat: prompt user to rotate to landscape on mobile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,15 @@ import { Grid } from './components/grid/Grid';
 
 export default function App() {
     return (
-        <div id="app">
-            <TopPanel />
-            <Grid />
-        </div>
+        <>
+            <div id="app">
+                <TopPanel />
+                <Grid />
+            </div>
+            <div id="rotate-prompt">
+                <p>Please rotate your device to landscape mode to play!</p>
+                <p>(You may need to turn off portrait lock)</p>
+            </div>
+        </>
     );
 }
